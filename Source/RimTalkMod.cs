d4173fee32a62eb85a7b91a623085b2f5cb79850
Dmytro Vostrikov <ustas@barco.kiev.ua>
2026-08-15 01:21:13 -0700

    Use Content.Name for the Mod Settings category label.

--- Source/RimTalkMod.cs
+++ Source/RimTalkMod.cs
@@ -36,7 +36,7 @@ namespace RimTalk.MemoryPatch
 
         public override string SettingsCategory()
         {
-            return "RimTalk-Expand Memory";
+            return Content?.Name ?? "RimTalk - Expand Memory";
         }
 
         public override void WriteSettings()
